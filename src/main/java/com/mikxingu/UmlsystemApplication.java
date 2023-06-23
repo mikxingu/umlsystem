392bbd3243a3487ac3e77b8e2981647fcc594375
Mike Alves <michelalvs@gmail.com>
2023-06-23 18:06:40 -0300

    Innitial Repo Commit

---
+++ src/main/java/com/mikxingu/UmlsystemApplication.java
@@ -0,0 +1,13 @@
+package com.mikxingu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmlsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikxingu/UmlsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikxingu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmlsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
