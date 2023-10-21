69a0142611e55edf1bcfeaefbb8b95866516c676
Mike Alves <michelalvs@gmail.com>
2023-10-21 12:52:36 -0300

    Added first domain class - Categoria

--- src/main/java/com/mikxingu/resources/CategoriaResource.java
+++ src/main/java/com/mikxingu/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.mikxingu.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mikxingu.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
-
+	
 	@GetMapping
-	public String listar() {
-		return "ENDPOINT CATEGORIAS IS WORKING!";
+	public List<Categoria> listCategories() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> categoriasList = new ArrayList<>();
+		
+		categoriasList.add(cat1);
+		categoriasList.add(cat2);
+		return categoriasList;
 	}
 }
