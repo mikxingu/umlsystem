da03dcbe1900054fcacd5b81ab2651c2f6d0d66c
Mike Alves <michelalvs@gmail.com>
2023-10-21 14:26:53 -0300

    IMplemented Categoria Repository and Serice

--- src/main/java/com/mikxingu/resources/CategoriaResource.java
+++ src/main/java/com/mikxingu/resources/CategoriaResource.java
@@ -1,28 +1,29 @@
 package com.mikxingu.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mikxingu.domain.Categoria;
+import com.mikxingu.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@GetMapping
-	public List<Categoria> listCategories() {
+	@Autowired private CategoriaService categoriaService;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> findById(@PathVariable Integer id) {
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria obj = categoriaService.find(id);
 		
-		List<Categoria> categoriasList = new ArrayList<>();
 		
-		categoriasList.add(cat1);
-		categoriasList.add(cat2);
-		return categoriasList;
+		return ResponseEntity
+				.ok()
+				.body(obj);
 	}
 }
---
+++ src/main/java/com/mikxingu/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.mikxingu.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mikxingu.domain.Categoria;
+import com.mikxingu.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		return obj.orElse(null);
+	}
+}
