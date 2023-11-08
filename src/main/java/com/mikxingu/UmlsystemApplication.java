1265786dd7d56eb0ae7532c754b74ecd6105ec83
Mike Alves <michelalvs@gmail.com>
2023-11-07 22:48:41 -0300

    Completed Lecture 63 - Instantiated ItemPedido and ItemPedidoPK

--- src/main/java/com/mikxingu/UmlsystemApplication.java
+++ src/main/java/com/mikxingu/UmlsystemApplication.java
@@ -13,6 +13,7 @@ import com.mikxingu.domain.Cidade;
 import com.mikxingu.domain.Cliente;
 import com.mikxingu.domain.Endereco;
 import com.mikxingu.domain.Estado;
+import com.mikxingu.domain.ItemPedido;
 import com.mikxingu.domain.Pagamento;
 import com.mikxingu.domain.PagamentoComBoleto;
 import com.mikxingu.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.mikxingu.repositories.CidadeRepository;
 import com.mikxingu.repositories.ClienteRepository;
 import com.mikxingu.repositories.EnderecoRepository;
 import com.mikxingu.repositories.EstadoRepository;
+import com.mikxingu.repositories.ItemPedidoRepository;
 import com.mikxingu.repositories.PagamentoRepository;
 import com.mikxingu.repositories.PedidoRepository;
 import com.mikxingu.repositories.ProdutoRepository;
@@ -41,18 +43,21 @@ public class UmlsystemApplication implements CommandLineRunner {
 	private CidadeRepository cidadeRepository;
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
-	@Autowired 
+
+	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UmlsystemApplication.class, args);
@@ -93,29 +98,45 @@ public class UmlsystemApplication implements CommandLineRunner {
 
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
-		
-		Endereco e1 = new Endereco(null, "Rua Flores",  "300", "Apto 303", "Jardim", "38220834", cli1, c1);
-		Endereco e2 = new Endereco(null, "Avenida Matos",  "105", "Sala 800", "Centro", "38777012", cli1, c2);
-		
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
-		
+
 		clienteRepository.saveAll(Arrays.asList(cli1));
-		
+
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
 		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
-		
+
 		Pagamento pgto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pgto1);
-		Pagamento pgto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		Pagamento pgto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
 		ped2.setPagamento(pgto2);
-		
+
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
-		
+
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pgto1, pgto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
 		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+		
+		
+
 	}
 
 }
